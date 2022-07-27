e718650028d018cde56f67305560a8fccf95bbf2
wata00913 <175d8639@gmail.com>
2022-07-27 16:38:41 +0900

    before_actionの国際化関連のメソッド名変更と辞書の階層構造を変更
    辞書のmessageは、confirmやalertなどの場合も含まれるので成功用の階層(success)を作った。
    また、メソッド名が不適切だったのでリネームをした。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,10 @@
 
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
-  before_action :common_locales
-  before_action :new_locales, only: %i[new create]
-  before_action :edit_locales, only: %i[edit update]
-  before_action :destroy_locales, only: :destroy
+  before_action :set_common_locales
+  before_action :set_new_and_create_locales, only: %i[new create]
+  before_action :set_edit_and_update_locales, only: %i[edit update]
+  before_action :set_destroy_locales, only: :destroy
 
   # GET /books
   # GET /books.json
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: success_message_locales }
+        format.html { redirect_to @book, notice: set_success_message_locale }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -46,7 +46,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: success_message_locales }
+        format.html { redirect_to @book, notice: set_success_message_locale }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
@@ -60,7 +60,7 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: success_message_locales }
+      format.html { redirect_to books_url, notice: set_success_message_locale }
       format.json { head :no_content }
     end
   end
@@ -77,27 +77,27 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :memo, :author, :picture)
   end
 
-  def common_locales
+  def set_common_locales
     @head_locales = t 'book.heading'
     @attr_locales = t 'book.attr'
     @link_locales = t 'book.action'
   end
 
-  def new_locales
+  def set_new_and_create_locales
     @submit_locales = t('book.submit.base', value: t('book.submit.new'))
-    @success_message_locales = t('book.message.success', deep_interpolation: true, action: t('book.message.success.create'))
+    @success_message_locale = t('book.message.success', deep_interpolation: true, action: t('book.message.success.create'))
   end
 
-  def edit_locales
+  def set_edit_and_update_locales
     @submit_locales = t('book.submit.base', value: t('book.submit.edit'))
-    @success_message_locales = t('book.message.success', deep_interpolation: true, action: t('book.message.success.update'))
+    @success_message_locale = t('book.message.success', deep_interpolation: true, action: t('book.message.success.update'))
   end
 
-  def destroy_locales
-    @success_message_locales = t('book.message.success', deep_interpolation: true, action: t('book.message.success.destroy'))
+  def set_destroy_locales
+    @success_message_locale = t('book.message.success', deep_interpolation: true, action: t('book.message.success.destroy'))
   end
 
-  def success_message_locales
-    @success_message_locales[:base]
+  def set_success_message_locale
+    @success_message_locale[:base]
   end
 end
