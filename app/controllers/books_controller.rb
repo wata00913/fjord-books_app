bb338054356d79322628cd8429e750a702f8b0b9
wata00913 <175d8639@gmail.com>
2022-07-28 11:19:46 +0900

    不要な`respond_to`を削除。
    現状は`json`  でのアクセスが不要のため削除した。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,10 +33,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: set_success_message_locale }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -47,10 +45,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: set_success_message_locale }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -61,7 +57,6 @@ class BooksController < ApplicationController
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: set_success_message_locale }
-      format.json { head :no_content }
     end
   end
 
