2a6d871fdde60506b2912ace715e687f4d3c6d6d
wata00913 <175d8639@gmail.com>
2022-08-11 09:21:31 +0900

    ユーザーの一覧画面と詳細画面を作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  def index
+    @users = User.order(:id)
+  end
+
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
