5f6642176f53f4579908b72fd984f045f1754547
wata00913 <175d8639@gmail.com>
2022-08-22 15:36:32 +0900

    メールアドレスのバリデーションを追加
    画面でのバリデーションと同様に@のみ検証。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :validate_email_format
+
+  private
+
+  def validate_email_format
+    errors.add(:email, 'メールアドレスに@を含めて下さい') unless email.include?('@')
+    errors.add(:email, '@に続く文字列に@を含めることはできません') if email.count('@') > 1
+  end
 end
