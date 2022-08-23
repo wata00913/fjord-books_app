333a142b556cd62e3fd6b75d631667b184e254c0
wata00913 <175d8639@gmail.com>
2022-08-23 10:26:40 +0900

    ユーザー一覧画面にページングと項目を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
+  DEFULAT_PER_PAGE = 25
 
   def index
-    @users = User.order(:id)
+    @users = User.order(:id).page(params[:page]).per(DEFULAT_PER_PAGE)
   end
 
   def show; end
