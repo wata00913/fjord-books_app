c2f0f2b5ba56210e86f7426b6978278eb212480f
wata00913 <175d8639@gmail.com>
2022-08-26 09:04:35 +0900

    パスワード再設定メールの送信内容をブラウザを使って確認できるように設定。
    letter_opener_webを使用。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :books
   root to: 'books#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
