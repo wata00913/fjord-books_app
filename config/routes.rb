58e1393974e11b1a2e5173c480d637234f93b918
wata00913 <175d8639@gmail.com>
2022-08-31 07:34:07 +0900

    Users::SessionsControllerが不要なのでファイルごと削除とそれに伴うdeviseのルーティング設定を修正
    削除理由はコミットID`c2256a7`と同じで不適切なパスを設定したため。
    コミットID`a00e55b`でブロック処理内で強制的にリダイレクトする必要はなかった。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions'  }
+  devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books
   root to: 'books#index'
