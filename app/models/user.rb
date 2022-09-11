0100a92deec94737200779293ea83a03d0c60db0
wata00913 <175d8639@gmail.com>
2022-09-12 07:25:02 +0900

    ユーザーモデルとフォロー関係モデルのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :following_relationships, class_name: 'FollowRelationship', foreign_key: :following_id
+  has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: :followed_id
+  has_many :followings, through: :following_relationships, source: :followed
+  has_many :followers, through: :follower_relationships, source: :following
 end
