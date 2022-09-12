4f25e665b46a3be9974b4ac8cf565f12ca30b562
wata00913 <175d8639@gmail.com>
2022-09-12 16:27:53 +0900

    Userモデルの項目を電話番号から住所へと置き換えと、それに伴う画面やスキーマ等の修正
    ユーザー情報の項目が要件通りではなかったため修正。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_10_230756) do
+ActiveRecord::Schema.define(version: 2022_09_12_074143) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 2022_08_10_230756) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "post_code"
-    t.string "phone_number"
     t.string "self_introduction"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
