53321f97d5d01de1b0710da6e8e23704ec1b7557
wata00913 <175d8639@gmail.com>
2022-09-15 15:58:21 +0900

    フォロー一覧画面とフォロワー一覧画面の表示ユーザーの並び順をフォロー順に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :following_relationships, class_name: 'FollowRelationship', foreign_key: :following_id
-  has_many :follower_relationships, class_name: 'FollowRelationship', foreign_key: :followed_id
+  has_many :following_relationships, -> { order(:created_at) },
+           class_name: 'FollowRelationship', foreign_key: :following_id
+  has_many :follower_relationships, -> { order(:created_at) },
+           class_name: 'FollowRelationship', foreign_key: :followed_id
   has_many :followings, through: :following_relationships, source: :followed
   has_many :followers, through: :follower_relationships, source: :following
 
