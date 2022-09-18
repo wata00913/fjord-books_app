8eab563ad240b02f911efe6ee115811c6ff24bb2
wata00913 <175d8639@gmail.com>
2022-09-18 10:58:34 +0900

    ユーザーを削除した際に紐づくフォロー関係も削除するようにアソシエーションのオプションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,11 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :following_relationships, -> { order(:created_at) },
-           class_name: 'FollowRelationship', foreign_key: :following_id
+           class_name: 'FollowRelationship', foreign_key: :following_id,
+           dependent: :destroy
   has_many :follower_relationships, -> { order(:created_at) },
-           class_name: 'FollowRelationship', foreign_key: :followed_id
+           class_name: 'FollowRelationship', foreign_key: :followed_id,
+           dependent: :destroy
   has_many :followings, through: :following_relationships, source: :followed
   has_many :followers, through: :follower_relationships, source: :following
 
