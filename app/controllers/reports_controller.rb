d620294b6798cd26b7604f81420e9a116b12c85d
wata00913 <175d8639@gmail.com>
2022-09-19 09:25:42 +0900

    ReportをScaffoldで作成。一部不要な処理、ファイルは削除済み。

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,58 @@
+class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
+  # GET /reports or /reports.json
+  def index
+    @reports = Report.all
+  end
+
+  # GET /reports/1 or /reports/1.json
+  def show; end
+
+  # GET /reports/new
+  def new
+    @report = Report.new
+  end
+
+  # GET /reports/1/edit
+  def edit; end
+
+  # POST /reports or /reports.json
+  def create
+    @report = Report.new(report_params)
+
+    if @report.save
+      redirect_to report_url(@report), notice: 'Report was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /reports/1 or /reports/1.json
+  def update
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: 'Report was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /reports/1 or /reports/1.json
+  def destroy
+    @report.destroy
+
+    redirect_to reports_url, notice: 'Report was successfully destroyed.'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.require(:report).permit(:title, :content, :posting_user_id)
+  end
+end
