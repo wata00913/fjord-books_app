b2bad672a51d1d2cc4b58a4e00ba9400831ca430
wata00913 <175d8639@gmail.com>
2022-09-20 15:32:27 +0900

    日報、コメント、コメント投稿者（User）の関連付けを追加。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
+  belongs_to :commenter, class_name: 'User', foreign_key: :posting_user_id, inverse_of: :comments
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,7 @@
 
 class Report < ApplicationRecord
   validates :title, :posting_user_id, presence: true
-  belongs_to :user, foreign_key: :posting_user_id,
-                    inverse_of: :reports
+  belongs_to :user, foreign_key: :posting_user_id, inverse_of: :reports
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :commenters, through: :comments, source: :user
 end
