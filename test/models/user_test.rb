63c0742dbedaf33bf0663f6d50210be15d594efe
wata00913 <175d8639@gmail.com>
2022-10-03 17:46:49 +0900

    Userモデルのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = User.where(name: 'Alice').first
+    @bob = User.where(name: 'Bob').first
+    @carol = User.where(name: 'Carol').first
+    @anonymous = User.where(email: 'sample-4@example.com').first
+  end
+
+  test 'Alice follow Bob' do
+    @alice.follow(@bob)
+
+    assert @alice.following?(@bob)
+    assert @bob.followed_by?(@alice)
+  end
+
+  test 'carol unfollow alice' do
+    @carol.unfollow(@alice)
+
+    assert_not @carol.following?(@alice)
+    assert_not @alice.followed_by?(@carol)
+  end
+
+  test 'return name if user name is presence' do
+    assert_equal 'Alice', @alice.name_or_email
+  end
+
+  test 'return email if user name is not presence' do
+    assert_equal 'sample-4@example.com', @anonymous.name_or_email
+  end
 end
