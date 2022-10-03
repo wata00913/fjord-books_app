c0be37811e0cc0158c99474c033d78de3cb0797b
wata00913 <175d8639@gmail.com>
2022-10-03 20:02:42 +0900

    Reportモデルのテストコードを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice_report = Report.where(title: '初日報').first
+  end
+
+  test 'self report is editable' do
+    alice = User.where(name: 'Alice').first
+
+    assert @alice_report.editable?(alice)
+  end
+
+  test 'other report is not editable' do
+    bob = User.where(name: 'Bob').first
+
+    assert_not @alice_report.editable?(bob)
+  end
 end
