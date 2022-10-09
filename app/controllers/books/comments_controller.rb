117494996c883f9a5616763313b1f8ec22044866
wata00913 <175d8639@gmail.com>
2022-10-09 11:18:47 +0900

    レビューの指摘箇所を修正。親のコメントコントローラーを削除。

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,19 +1,34 @@
 # frozen_string_literal: true
 
-class Books::CommentsController < CommentsController
+class Books::CommentsController < ApplicationController
   before_action :set_book, only: %i[create destroy]
 
   def create
-    super(@book)
+    @comments = @book.comments
+    @new_comment = @comments.build(comment_params)
+    @new_comment.commenter = current_user
+
+    flash[:alert] = @new_comment.errors.full_messages.to_sentence unless @new_comment.save
+
+    redirect_to @book
   end
 
   def destroy
-    super(@book, params[:id])
+    comment_id = params[:id]
+    @book.comments.find(comment_id).destroy
+    # 削除されたコメントはcommentsオブジェクトに残ったままなので同期化する
+    @book.comments.reload
+
+    flash[:notice] = t('controllers.common.notice_destroy', name: Comment.model_name.human)
 
     redirect_to @book
   end
 
   private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 
   def set_book
     @book = Book.find(params[:book_id])
--- app/controllers/reports/comments_controller.rb
+++ app/controllers/reports/comments_controller.rb
@@ -1,19 +1,34 @@
 # frozen_string_literal: true
 
-class Reports::CommentsController < CommentsController
+class Reports::CommentsController < ApplicationController
   before_action :set_report, only: %i[create destroy]
 
   def create
-    super(@report)
+    @comments = @report.comments
+    @new_comment = @comments.build(comment_params)
+    @new_comment.commenter = current_user
+
+    flash[:alert] = @new_comment.errors.full_messages.to_sentence unless @new_comment.save
+
+    redirect_to @report
   end
 
   def destroy
-    super(@report, params[:id])
+    comment_id = params[:id]
+    @report.comments.find(comment_id).destroy
+    # 削除されたコメントはcommentsオブジェクトに残ったままなので同期化する
+    @report.comments.reload
+
+    flash[:notice] = t('controllers.common.notice_destroy', name: Comment.model_name.human)
 
     redirect_to @report
   end
 
   private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 
   def set_report
     @report = Report.find(params[:report_id])
