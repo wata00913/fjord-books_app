069dc7d4424626b08cb9cfeb42a6880c351f6b3d
wata00913 <175d8639@gmail.com>
2022-10-13 16:12:14 +0900

    レビュー指摘箇所を修正。followed_idカラムにインデックスを再追加
    フォロワーに関する問い合わせがSCAN TABLEであるためインデックスを追加してパフォーマンスを改善

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_135407) do
+ActiveRecord::Schema.define(version: 2022_10_13_070619) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_10_06_135407) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
     t.index ["following_id", "followed_id"], name: "index_follow_relationships_on_following_id_and_followed_id", unique: true
   end
 
