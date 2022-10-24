a8b4eed7546589e02abbbb00bd9373af1f0de120
wata00913 <175d8639@gmail.com>
2022-10-24 15:38:22 +0900

    click_onを使った処理に統一

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -44,10 +44,7 @@ class BooksTest < ApplicationSystemTestCase
   test 'updating a Book' do
     visit books_url
 
-    # click_onでは本の編集リンクを特定出来ない。そこで下記のような処理を行う。
-    # 本一覧の先頭行を特定し、その行の編集リンクをクリックする
-    book_line = all('tbody tr')[0]
-    book_line.click_on '編集'
+    click_on '編集', match: :prefer_exact
 
     fill_in 'メモ', with: @book.memo
     fill_in 'タイトル', with: @book.title
