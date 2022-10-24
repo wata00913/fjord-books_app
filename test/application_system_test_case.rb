7265ebdeb07c9c974127d231ec515ca5835033b0
wata00913 <175d8639@gmail.com>
2022-10-24 15:04:17 +0900

    定数を削除
    参照箇所が少ないためハードコーディングでも問題ないと判断。

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,13 +5,11 @@ require 'test_helper'
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
-  COMMON_TEST_PASSWORD = 'password'
-
   def login_as(user)
     visit root_url
 
     fill_in 'Eメール', with: user.email
-    fill_in 'パスワード', with: COMMON_TEST_PASSWORD
+    fill_in 'パスワード', with: 'password'
 
     click_on 'ログイン'
   end
